93090a5ef61bb6239b2f1e2ff48502dc9da259a5
Luke Gronert <lukegronert@gmail.com>
2022-04-21 11:30:16 +0800

    Added getCoinData function that checks for coinData in local storage, then checks if its less than 2 minutes old. If older than 2 min, fetches new data. If not, returns the data in localstorage. Fed that through to Dashboard.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import { NestCamWiredStandTwoTone } from "@mui/icons-material";
+import React, {useState} from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
 
 export default function App() {
+  const [coinData, setCoinData] = useState([]);
+
+  const getCoinData = () => {
+    // Check if coinData exists in local storage or if it is older than 2 minutes (120,000ms)
+    if(localStorage.getItem('coinData') === null || (new Date().getTime() - JSON.parse(localStorage.getItem('coinData')).timestamp) > 120000) {
+      fetch('https://api.coincap.io/v2/assets')
+        .then(response => response.json())
+        .then(data => {
+          localStorage.setItem('coinData', JSON.stringify(data))
+          console.log('fetched')
+        })
+    } else {
+      console.log(JSON.parse(localStorage.getItem('coinData')));
+      console.log('local storage')
+    }
+  }
+
   return (
     <Router>
       <div>
@@ -17,7 +34,7 @@ export default function App() {
             renders the first one that matches the current URL. */}
         <Routes>
           <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard coinData={coinData} setCoinData={setCoinData} getCoinData={getCoinData} />} />
           <Route path="/" element={<LandingPage />} />
         </Routes>
       </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,12 @@
 import MainNav from '../components/MainNav';
 import DashboardNav from '../components/DashboardNav';
 
-export default function Dashboard() {
+export default function Dashboard({coinData, setCoinData, getCoinData}) {
     return (
         <section>
             <MainNav />
             <DashboardNav />
+            <button onClick={() => getCoinData()}>GetCoinData</button>
         </section>
     )
 }
