0f119d67fe58ef444b54f5059ae91061bac71929
lukegronert@gmail.com <lukegronert@gmail.com>
2022-05-10 02:49:28 -0700

    Changed mainNav to material ui AppBar.

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,28 +1,199 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import netlifyIdentity from 'netlify-identity-widget';
+// import '../css/mainNav.css';
+
+// export default function LandingPageNav({user}) {
+    
+    //     return (
+        //         <nav>
+        //             <ul>
+        //                 <li>
+        //                     <Link to="/">Home</Link>
+        //                 </li>
+        //                 <li>
+        //                     <Link to="/about">About</Link>
+        //                 </li>
+        //                 <li>
+        //                     <Link to="/Dashboard">Dashboard</Link>
+        //                 </li>
+        //                 <li>
+        //                     <a className="login-link" onClick={() => netlifyIdentity.open()}>Login</a>
+        //                 </li>
+        //                 <li>
+        //                     <button onClick={() => console.log(user)}>USER</button>
+        //                 </li>
+        //             </ul>
+        //       </nav>
+        //     );
+        // }
+        
+import * as React from 'react';
+import {Link} from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import netlifyIdentity from 'netlify-identity-widget';
+
 import '../css/mainNav.css';
 
-export default function LandingPageNav({user}) {
-    
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/Dashboard">Dashboard</Link>
-                </li>
-                <li>
-                    <a className="login-link" onClick={() => netlifyIdentity.open()}>Login</a>
-                </li>
-                <li>
-                    <button onClick={() => console.log(user)}>USER</button>
-                </li>
-            </ul>
-      </nav>
-    );
-}
+export default function MainNav ({user}) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              <MenuItem key='about' onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                      <Link to="/about" >About</Link>
+                  </Typography>
+              </MenuItem>
+              <MenuItem key='dashboard' onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                      <Link to="/dashboard">Dashboard</Link>
+                  </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                key='about'
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Link to="/about">About</Link>
+              </Button>
+              <Button
+                key='dashboard'
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Link to="/dashboard">Dashboard</Link>
+              </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar>B</Avatar>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+                <MenuItem key='logout' onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center" onClick={() => netlifyIdentity.open()}>Login/Logout</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
