1178c023dfa0db584d7cb37bff8766431c2860e7
lukegronert@gmail.com <lukegronert@gmail.com>
2022-05-22 20:47:19 -0700

    Removed useless commented out code. Created color palette. Centered title on landing page.

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,32 +1,4 @@
-// import { Link } from 'react-router-dom';
-// import netlifyIdentity from 'netlify-identity-widget';
-// import '../css/mainNav.css';
-
-// export default function LandingPageNav({user}) {
-    
-    //     return (
-        //         <nav>
-        //             <ul>
-        //                 <li>
-        //                     <Link to="/">Home</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to="/about">About</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to="/Dashboard">Dashboard</Link>
-        //                 </li>
-        //                 <li>
-        //                     <a className="login-link" onClick={() => netlifyIdentity.open()}>Login</a>
-        //                 </li>
-        //                 <li>
-        //                     <button onClick={() => console.log(user)}>USER</button>
-        //                 </li>
-        //             </ul>
-        //       </nav>
-        //     );
-        // }
-        
+       
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,12 @@
 import MainNav from '../components/MainNav';
+import '../css/landingPage.css'
 export default function LandingPage({user}) {
     return (
         <section>
             <MainNav user={user} />
-            LANDING PAGE
+            <div className="h1Container">
+                <h1 className='landingPageh1'>CRYPTO</h1>
+            </div>
         </section>
     )
 }
