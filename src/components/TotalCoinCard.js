a5c58cb8b936e66964e01a3ebce5e77bade6d788
Luke Gronert <lukegronert@gmail.com>
2022-06-05 15:07:27 +0800

    Added DonutSmallIcon to coinCard.

--- src/components/TotalCoinCard.js
+++ src/components/TotalCoinCard.js
@@ -1,11 +1,12 @@
+import DonutSmallIcon from '@mui/icons-material/DonutSmall';
 import '../css/coinCard.css';
 
 export default function TotalCoinCard({coin, amount, total}) {
     return (
         <div className="coinCard">
             <h2>{coin}</h2>
-            <p>{amount}</p>
-            <p>{total}</p>
+            <p>{amount} <DonutSmallIcon style={{ verticalAlign: 'middle'}} /></p>
+            <p>${total}</p>
         </div>
     )
 }
