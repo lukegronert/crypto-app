9e7d7435a4069800e6e46f830e2194361baf7d49
Luke Gronert <lukegronert@gmail.com>
2022-06-07 11:55:57 +0800

    Created leaderboard table and styled it.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,7 +1,18 @@
 import {useState, useEffect} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import '../css/leaderboard.css';
 
 export default function Leaderboard({coinData, setCoinData, doc}) {
     const [leaderboard, setLeaderboard] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     const getUserCoinTotals = async () => {
         const sheet1 = doc.sheetsByIndex[0];
         const sheet1Rows = await sheet1.getRows();
@@ -25,29 +36,66 @@ export default function Leaderboard({coinData, setCoinData, doc}) {
             })
             leaderboardArray.push(userBank);
         }
-        leaderboardArray = leaderboardArray.sort((a,b) => a.total - b.total);
+        leaderboardArray = leaderboardArray.sort((a,b) => b.total - a.total);
         setLeaderboard(leaderboardArray)
+        setIsLoading(false)
     }
 
+    
     useEffect(() => {
         fetch('https://api.coincap.io/v2/assets?limit=20')
-            .then(response => response.json())
-            .then(data => {
-                setCoinData(data.data)
-            })
+        .then(response => response.json())
+        .then(data => {
+            setCoinData(data.data)
+        })
     }, [])
+    
+    useEffect(() => {
+        console.log('rerender')
+    }, [isLoading])
 
     useEffect(() => {
         getUserCoinTotals()
     }, [coinData])
 
-    return (
-        <section>
-            {leaderboard.map(user => {
-                return (
-                    <div key={user.userName}>{user.userName} - {user.total}</div>
-                )
-            })}
-        </section>
-    )
+    if(isLoading) {
+        return (
+            <div>Loading...</div>
+        )
+    } else {
+        return (
+            // <section>
+            //     {leaderboard.map((user, index) => {
+            //         return (
+            //             <div key={user.userName}>#{index+1} {user.userName} - {user.total}</div>
+            //         )
+            //     })}
+            // </section>
+            <TableContainer component={Paper} style={{maxWidth: '650px', margin: 'auto'}} className="leaderboardTable" >
+                    <Table sx={{ maxWidth: 650, backgroundColor: '#B2DBEB' }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow >
+                                <TableCell></TableCell>
+                                <TableCell align="left">Name</TableCell>
+                                <TableCell align="right">Value</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {leaderboard.map((row, index) => (
+                            <TableRow
+                            key={row.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    #{index+1}
+                                </TableCell>
+                                <TableCell align="left"><span className="rowValue">{row.userName}</span></TableCell>
+                                <TableCell align="right"><span className="rowValue">${Number(row.total).toFixed(2)}</span></TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+        )
+    }
 }
