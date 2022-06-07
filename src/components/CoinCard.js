36df8b3de4f25aa048304dd7f82dcb6ab3f97a3f
Luke Gronert <lukegronert@gmail.com>
2022-06-07 11:55:16 +0800

    Adjusted layout of card so middle value is in exact center of card.

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -4,9 +4,15 @@ import '../css/coinCard.css';
 export default function CoinCard({coin, amount, total}) {
     return (
         <div className="coinCard">
-            <h2 className="coinCardSymbol">{coin}</h2>
-            <p className="coinCardAmount">{amount} <DonutSmallIcon style={{ verticalAlign: 'middle', marginTop: '-3px'}} /></p>
-            <p className="coinCardTotal">${Number(total).toFixed(2)}</p>
+            <div className="coinCardBox">
+                <h2 className="coinCardSymbol">{coin}</h2>
+            </div>
+            <div className="coinCardBox">
+                <p className="coinCardAmount">{amount} <DonutSmallIcon style={{ verticalAlign: 'middle', marginTop: '-3px'}} /></p>
+            </div>
+            <div className="coinCardBox">
+                <p className="coinCardTotal">${Number(total).toFixed(2)}</p> 
+            </div>
         </div>
     )
 }
