2391f05ac24ca5545501f4884c611a70fd4df220
Luke Gronert <lukegronert@gmail.com>
2022-06-16 10:57:15 +0800

    Changed Account avatar letter to A

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -145,7 +145,7 @@ export default function MainNav ({user}) {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar>B</Avatar>
+                <Avatar>A</Avatar>
               </IconButton>
             </Tooltip>
             <Menu
