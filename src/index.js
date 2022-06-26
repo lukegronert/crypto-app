6760b4303a455e7b0c7ed1a27cc81beba82c359a
Luke Gronert <lukegronert@gmail.com>
2022-06-25 22:25:04 -0700

    Made font-family sans-serif for the whole website

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import netlifyIdentity from 'netlify-identity-widget';
+import './css/index.css';
 
 
 netlifyIdentity.init();
